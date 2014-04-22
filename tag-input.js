dfa8217da2c94263202eba836ce9b98c3b842231
John Wu <webmaster@leapoahead.com>
2014-04-22 19:38:27 +0800

    regular

--- tag-input.js
+++ tag-input.js
@@ -1,10 +1,14 @@
+/**
+* @fileOverview This file has functions related to documenting JavaScript.
+* @author <a href="mailto:"></a>
+* @version 1.0.1
+*/
+
 (function(window) {
   /* variables */
   var document = window.document,
       inputs = document.getElementsByTagName('input'),
-      tagstore = {},
-      correspond = {},
-      core = {},
+      tagInputs = [],
       ui = {},
       util = {};
 
@@ -22,47 +26,57 @@
   };
 
   ui.cloneInput = function(el) {
-    var ret = el.cloneNode(true);
+    // do not use deep clone
+    var ret = el.cloneNode(false);
     ret.name = "__" + el.name;
-    ret.id = "__" + el.id;
+    if (!!ret.id) {
+      ret.id = "__" + el.id;
+    }
     ret.removeAttribute('data-input-tag');
+    ui.wrapSpan(ret); // a relative positioned inline wrapper
     return ret;
   };
 
-  core.createNewTag = function() {
-    tagstore[this.name].push(this.value);
-    this.value;
+  ui.wrapSpan = function(el) {
+    var span = document.createElement('span');
+    span.style.position = "relative";
+    span.appendChild(el);
+    return span;
   };
 
-  core.deleteTag = function() {
-
+  ui.prependSibling = function(el, sib) {
+    el.parentNode.insertBefore(sib, el);
   };
 
-  core.process = function(el) {
-    util.addEventListener(el, 'blur', core.createNewTag);
-    util.addEventListener(el, 'keyup', function(event) {
-      if (event.keyCode === 13) {
-        // the keyCode of the Enter key is 13
-        core.createNewTag.call(this);
-      }
-      else if (event.keyCode === 46) {
-        // the keyCode of the Delete key is 46
-        core.deleteTag.call(this);
-      }
-    });
+  /**
+   *  Tag
+   *  @constructor
+   *  @lends TagInputControl
+   */
+  function Tag() {
+
+  }
+
+  /**
+   *  TagInput
+   *  @constructor
+   *  @param {object} oriInput The original DOM Object
+   *  @lends TagInputControl
+   */
+  function TagInput(oriInput) {
+    this.attach(oriInput);
+  }
+
+  TagInput.prototype.attach = function(oriInput) {
+    this.oriInput = oriInput; // the original dom
+    this.tagInput = ui.cloneInput(this.oriInput); // clone the dom node, rename the `name` and `id` property
+    ui.prependSibling(this.oriInput, this.tagInput.parentNode); // add the tagInput's wrapper to DOM tree
+    this.tags = []; // array of tag names
+    ui.hideDOM(oriInput); // the original dom should be hidden
   };
 
-  core.tagInput = function(el) {
-    var parent = el.parentNode,
-        elTag = ui.cloneInput(el);
-    parent.insertBefore(elTag, el);
-    tagstore[el.name] = [];
-    correspond[el.name] = el;
-    ui.hideDOM(el);
-    core.process(elTag);
-  };
 
-  // filter inputs
+  // inputs filter
   var i;
   inputs = Array.prototype.slice.call(inputs, 0, inputs.length);
   for (i = 0; i < inputs.length; i ++) {
@@ -71,7 +85,7 @@
       !!inputs[i].type &&
       inputs[i].type === 'text' &&
       !!inputs[i].name) {
-      core.tagInput(inputs[i]);
+      tagInputs.push(new TagInput(inputs[i]));
     }
   }
 
